bc4ba4ebc2d04e88904b1805c3ea2508f3e554bb
KooshaPari <42529354+KooshaPari@users.noreply.github.com>
2023-10-20 22:54:11 -0700

    Update mobility.m

--- mobility.m
+++ mobility.m
@@ -46,55 +46,5 @@ classdef mobility
         end
         brick.StopAllMotors();
         end
-        % Goals: Rotate Sensor in 90deg intervals checking each possible
-        % direction for open routes. 
-        % Each open route should be noted in an array then chosen with
-        % backwards always being lowest prio.
-        % New Goals: Scan 360deg 24/7 and use a mathematical modal to only
-        % take the scans at 90deg intervals, using that to then self adjust
-        % the car through slight speed changes in each motor.
-        function scan
-            % Basic Setup - Gyro Calibration, Angle Data Collection,
-            brick.GyroCalibrate(2);
-            curr = brick.GyroAngle(2); 
-            paths = [];
-
-            % Loop until 360 degree turn
-            while abs(curr-brick.GyroAngle(2)) ~= 359
-                %Relative Angle initialization and take current angle
-                angle = 0;
-                cur = brick.GyroAngle(2); 
-
-                %Scan in 90 degree intervals
-                while angle >=-80
-                       brick.MoveMotor('D',50);
-                       angle = brick.GyroAngle(2)-cur;
-                end
-                brick.StopAllMotors();
-                %Take distance and check if open, if open add to array
-                distance1 = brick.UltrasonicDist(1);
-                if(distance1>30)
-                    paths(end+1) = cur;
-                end
-            end
-                % Choose route
-                i=0;
-                route = 0;
-                len = size(paths);
-                while i<len
-                    if paths(i) ~= 180
-                        route = paths(i);
-                    end
-                    if i == len-1 && route == 0
-                        route = 180;
-                    end
-                    i = i+1;
-                end
-                % Turn to set route
-                while angle>=route
-                    brick.MoveMotor('D',50);
-                    angle = brick.GyroAngle(2);
-                end
-        end
     end
 end
