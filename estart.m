85121d7bbfd793d89b72e0d6f6aa27aa57a3528b
KooshaPari <kooshapari@gmail.com>
2023-11-16 20:50:22 -0700

    clean-up

--- estart.m
+++
@@ -1,6 +0,0 @@
-% Connect to Brick.
-brick = ConnectBrick('EPIKS');
-% Play tone with frequency 800Hz and duration of 500ms.
-brick.playTone(100, 800, 500);
-% Get current battery level.
-v = brick.GetBattVoltage()
